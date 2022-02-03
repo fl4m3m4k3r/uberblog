557186c732c492718e97ab6aa95e6cfb9f837001
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2022-02-04 01:13:46 +0300

    comment model, setup comments for posts. frontend enhancement

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_130215) do
+ActiveRecord::Schema.define(version: 2022_02_03_131258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -36,5 +43,6 @@ ActiveRecord::Schema.define(version: 2022_02_03_130215) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
